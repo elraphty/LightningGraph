0b9a69d15289d9315608d3bdcac22f4b3052e7ba
elraphty <elraphtyofficial@gmail.com>
2022-04-27 09:32:32 +0100

    added updateee graph functionality

--- components/GraphScene.tsx
+++ components/GraphScene.tsx
@@ -1,28 +1,26 @@
 import React, { useEffect, useRef } from "react";
-// import { useSocket } from "../../hooks/UseSocket";
-// import { useApi } from "../../hooks/UseApi";
+import { useSocket } from "../hooks/useSocket";
 import { Lnd } from "../interfaces/LndRestTypes";
 import { Graph } from "./Graph";
 import axios from 'axios';
 
 
 export const GraphScene = () => {
-    // const api = useApi();
     const graphRef = useRef<Graph>(null);
 
     useEffect(() => {
         const getGraph = async () => {
             const res = await axios.get('http://127.0.0.1:8001/api/graph', { headers: { credentials: "include" } });
-            console.log('Res ===', res.data);
             const d: Lnd.Graph = res.data;
+
             if(graphRef.current) graphRef.current.createGraph(d);
         }
         getGraph();
     }, []);
 
-    // useSocket("graph", (update: Lnd.GraphUpdate) => {
-    //     // Exercise: Call `graphRef.current.updateGraph` with the update
-    // });
+    useSocket("graph", (update: Lnd.GraphUpdate) => {
+        if(graphRef.current) graphRef.current.updateGraph(update);
+    });
 
     return (
         <div className="container-fluid h-100">
---
+++ hooks/useSocket.ts
@@ -0,0 +1,41 @@
+import { useContext, useEffect } from "react";
+import { SocketContext } from "../components/SocketContext";
+
+/**
+ * Listens to the websocket channel and calls the handler for each event
+ * received on the channel.
+ * @param channel name of the channel, ex: graph
+ * @param handler event handler fired when channel data is received
+ */
+export function useSocket<T>(channel: string, handler: (data: T) => void) {
+    // Obtain the socket from the context
+    const { socket } = useContext(SocketContext);
+
+    // Attach the listener to the socket when the
+    useEffect(() => {
+        // Construct a listener function that parses the event from JSON
+        // and calls the handler with the data if we are subscribing to
+        // the requested channel.
+        console.log("Adding listener for channel:", channel);
+        const listener = async (e: { data: { text: () => any; }; }) => {
+            try {
+                const text = await e.data.text();
+                const obj = JSON.parse(text);
+                if (obj.channel == channel) {
+                    handler(obj.data);
+                }
+            } catch (ex) {
+                console.error("Failed to parse ws payload", ex);
+            }
+        };
+        if (socket) {
+            socket.addEventListener("message", listener);
+
+            // Remove the handler from the socket when the component unmounts
+            return () => {
+                console.log("Removing listener for channel:", channel);
+                socket.removeEventListener("message", listener);
+            };
+        }
+    }, [channel, handler, socket]);
+}
